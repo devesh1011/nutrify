8ab8e44f0b9fbd3cf3464d215bf273d76a6a407f
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2024-12-18 17:07:33 +0530

    change in model configuration

--- models/openai_model.py
+++ models/openai_model.py
@@ -1,9 +1,8 @@
-from langchain_openai import ChatOpenAI
 import os
+from langchain_openai import ChatOpenAI
 
 def get_openai_model():
-    """
-    Initializes the OpenAI model for use with LangChain.
-    :return: ChatOpenAI model instance
-    """
-    return ChatOpenAI(model="gpt-4o-mini", api_key=os.getenv("OPENAI_API_KEY"))
+    return ChatOpenAI(
+        model="gpt-4o-mini",
+        api_key=os.getenv("OPENAI_API_KEY"), temperature=0.2
+    )
