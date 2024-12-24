9ab414577d3351166b4b553274305853d99a254d
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2024-12-24 09:21:50 +0530

    changed the chat model to NVIDIACHAT

--- models/openai_model.py
+++ models/openai_model.py
@@ -1,8 +1,11 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+
 
 def get_openai_model():
-    return ChatOpenAI(
-        model="gpt-4o-mini",
-        api_key=os.getenv("OPENAI_API_KEY"), temperature=0.2
+    return ChatNVIDIA(
+        model="meta/llama-3.3-70b-instruct",
+        api_key=os.getenv("NVIDIA_API_KEY"),
+        temperature=0.1,
     )
