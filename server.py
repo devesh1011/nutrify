b9d8cf22180cc440c2416bff331d9676fd486423
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2024-12-26 20:34:47 +0530

    file upload code added

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from app.main import main
 import json
+from PIL import Image
+import easyocr
 
 app = FastAPI(
     title="Nutrify API",
@@ -19,10 +22,37 @@ def home():
     return "Hello World!"
 
 
-@app.post("/analyze", summary="Analyze food ingredients")
-async def analyze_ingredients(request: IngredientRequest):
+@app.post("/upload", summary="Image upload route", tags=["Image Upload"])
+async def handle_uploads(file: UploadFile = File(...)):
+    """
+    Handles image uploads for OCR processing. Extracts text from the image and returns it.
+    """
     try:
-        content = main(request.ingredients)
+        # Verify uploaded file is an image
+        if file.content_type not in ["image/jpeg", "image/png"]:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid file type. Only JPEG and PNG are supported.",
+            )
+
+        # Load the uploaded image
+        image = Image.open(file.file)
+
+        reader = easyocr.Reader(["en"], gpu=False)
+        result = reader.readtext(image)
+
+        ingredients = [item[1] for item in result]
+
+        return analyze_ingredients("".join(ingredients))
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
+
+
+@app.post("/analyze", summary="Analyze food ingredients")
+async def analyze_ingredients(ingredients: IngredientRequest):
+    try:
+        content = main(ingredients)
         try:
             # Strip unwanted characters like ```json or ```
             content_json = json.loads(content.strip("```json").strip("```"))
