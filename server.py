7650cef654bcff8b7bb8ea6759580c562f6f0aaf
devesh1011 <116812354+devesh1011@users.noreply.github.com>
2025-01-25 16:08:50 +0530

    refactor: modularize server routes and simplify main server configuration

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
-from fastapi import FastAPI, File, HTTPException, UploadFile
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from app.main import main
-import json
-from PIL import Image
+from fastapi import FastAPI
 from langserve import add_routes, RemoteRunnable
+from routes.ingredient_routes import router
 
 app = FastAPI(
     title="Nutrify API",
@@ -12,52 +8,13 @@ app = FastAPI(
     version="1.0.0",
 )
 
+# Include the router
+app.include_router(router, prefix="/api/v1")
 
-class IngredientRequest(BaseModel):
-    ingredients: str
-
-
-@app.get("/", summary="Home Route")
-def home():
-    return "Hello World!"
-
-
-@app.post("/upload", summary="Image upload route", tags=["Image Upload"])
-async def handle_uploads(file: UploadFile = File(...)):
-    """
-    Handles image uploads for OCR processing. Extracts text from the image and returns it.
-    """
-    try:
-        # Verify uploaded file is an image
-        if file.content_type not in ["image/jpeg", "image/png"]:
-            raise HTTPException(
-                status_code=400,
-                detail="Invalid file type. Only JPEG and PNG are supported.",
-            )
-
-        # Load the uploaded image
-        image = Image.open(file.file)
-        return None
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
-
-
-@app.post("/analyze", summary="Analyze food ingredients")
-async def analyze_ingredients(ingredients: IngredientRequest):
-    try:
-        content = main(ingredients.ingredients)
-        # print(content)
-        # Strip unwanted characters like ```json or ``` and parse JSON
-        content_json = json.loads(content.strip("```json").strip("```"))
-        return content_json
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
+# LangServe routes
 runnable = RemoteRunnable("http://localhost:8080")
 add_routes(app, runnable)
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, port=8000)
